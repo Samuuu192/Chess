664b35e42ddfd8843c76250736d66fded50f28f2
Samuel <samuelbds1@outlook.com>
2022-10-21 01:21:42 -0300

    Adição do fullscreen

--- game/Game.java
+++ game/Game.java
@@ -1,6 +1,10 @@
 package chess.game;
 
 import javafx.application.Application;
+
+import javafx.stage.Screen;
+import javafx.geometry.Rectangle2D;
+
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 import javafx.scene.Scene;
@@ -11,8 +15,9 @@ public class Game extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
+			Rectangle2D size = Screen.getPrimary().getBounds();
 			AnchorPane root = (AnchorPane)FXMLLoader.load(getClass().getResource("../stuff/GameScreen.fxml"));
-			Scene scene = new Scene(root,1366,768);
+			Scene scene = new Scene(root,size.getMaxX(),size.getMaxY());
 			scene.getStylesheets().add(getClass().getResource("../stuff/GameStyle.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.resizableProperty().setValue(Boolean.FALSE);
